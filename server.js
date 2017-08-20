2349bf5eee82df9a6a63bd50d3665ed6a18f3c20
Abhi Singh <abhidashing.singh@gmail.com>
2017-08-20 19:44:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ var config={
 };
 var app = express();
 app.use(morgan('combined'));
-var pool=new Pool(config);
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool=new Pool(config);
 app.get('/article',function(req,res){
     pool.query("SELECT * FROM Test", function(err,result){
         if(err){

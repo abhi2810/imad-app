a77551ee51d9f6444b9f5b7942dd4a255bb3437f
Abhi Singh <abhidashing.singh@gmail.com>
2017-08-20 19:55:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/article',function(req,res){
         if(err){
             res.status(500).send(err.toString());
         }else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });

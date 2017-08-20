a83156ef232280982796f64e4b58b05e968f53d5
Abhi Singh <abhidashing.singh@gmail.com>
2017-08-20 19:47:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 });
 var pool=new Pool(config);
 app.get('/article',function(req,res){
-    pool.query("SELECT * FROM Test", function(err,result){
+    pool.query("SELECT * FROM 'Test'", function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
